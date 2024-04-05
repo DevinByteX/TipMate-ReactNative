46ed35df266a92709a18c8f23be63b1fcbca8ed7
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-05 20:30:08 +0530

    absolute path settings configured

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./app'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './app',
+          "@components": "./app/components",
+        }
+      }
+    ]
+  ]
 };
