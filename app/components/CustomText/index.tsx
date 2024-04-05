e9d147c126b3366314deac3b69fb1ec451175a9c
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-05 20:27:05 +0530

    export method changed

--- app/components/CustomText/index.tsx
+++ app/components/CustomText/index.tsx
@@ -1,15 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const CustomText = () => {
+export const CustomText = () => {
   return (
     <View>
       <Text style={styles.textStyles}>CustomText</Text>
     </View>
   );
 };
-
-export default CustomText;
 
 const styles = StyleSheet.create({
   textStyles: {
