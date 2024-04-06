d3e2129acb316ffce7da719edf3311afab689d02
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-06 16:13:25 +0530

    essential comments added for future references

--- babel.config.js
+++ babel.config.js
@@ -8,8 +8,8 @@ module.exports = {
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
           '@': './app',
-          "@components": "./app/components",
-          "@styles": "./app/styles"
+          "@components": "./app/components/index", // if you are refering common index file in folder you should add "/index" end of the path
+          "@styles": "./app/styles/"  // this is working either we add end "/" or not to the path, for safe side i have added end "/" to the path
         }
       }
     ]
