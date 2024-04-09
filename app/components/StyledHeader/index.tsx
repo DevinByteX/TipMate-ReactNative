e4be75d57d0bfcf99d0bc64166e4216d5fac8fac
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-10 01:04:22 +0530

    Header partially completed

--- app/components/StyledHeader/index.tsx
+++ app/components/StyledHeader/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StatusBar, Text, View} from 'react-native';
+import {
+  UnistylesRuntime,
+  createStyleSheet,
+  useStyles,
+} from 'react-native-unistyles';
 
 type styledHeaderProps = {};
 
 export const StyledHeader = ({}: styledHeaderProps) => {
+  const {styles, theme} = useStyles(stylesheet);
+
   return (
-    <View>
-      <Text>StyledHeader</Text>
+    <View style={styles.mainContainer}>
+      <StatusBar
+        backgroundColor={theme.colors.headerBGColor}
+        translucent={true}
+        barStyle={
+          UnistylesRuntime.colorScheme === 'dark'
+            ? 'light-content'
+            : 'dark-content'
+        }
+      />
+      <View style={styles.headerMainContainer}>
+        <View style={styles.headerInnerContainer}></View>
+      </View>
     </View>
   );
 };
+
+const stylesheet = createStyleSheet(({colors}) => ({
+  mainContainer: {
+    backgroundColor: colors.headerBGColor,
+    zIndex: 999,
+  },
+  headerMainContainer: {
+    marginTop: UnistylesRuntime.statusBar.height,
+    height: UnistylesRuntime.statusBar.height,
+    backgroundColor: colors.headerBGColor,
+  },
+  headerInnerContainer: {},
+}));
--- app/styles/themes.ts
+++ app/styles/themes.ts
@@ -1,16 +1,16 @@
 const sharedColors = {
     barbie: '#ff9ff3',
-    oak: '#1dd1a1',
+    blood: '#ff6b6b',
     sky: '#48dbfb',
-    fog: '#c8d6e5',
-    aloes: '#00d2d3',
-    blood: '#ff6b6b'
+    primary_light: '#E3E9EC',
+    primary_dark: '#333333'
 }
 
 export const lightTheme = {
     colors: {
         ...sharedColors,
         backgroundColor: '#ffffff',
+        headerBGColor: sharedColors.primary_light,
         typography: '#000000',
         accent: sharedColors.blood
     },
@@ -27,6 +27,7 @@ export const darkTheme = {
     colors: {
         ...sharedColors,
         backgroundColor: '#000000',
+        headerBGColor: sharedColors.primary_dark,
         typography: '#ffffff',
         accent: sharedColors.barbie
     },
@@ -43,6 +44,7 @@ export const premiumTheme = {
     colors: {
         ...sharedColors,
         backgroundColor: sharedColors.barbie,
+        headerBGColor: sharedColors.sky,
         typography: '#76278f',
         accent: '#000000'
     },
