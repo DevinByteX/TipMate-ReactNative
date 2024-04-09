bb364e2b320c253881ef6f25a1be0a97aa440efd
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-10 01:43:17 +0530

    some styles added

--- app/screens/TipScreens/HomeTipScreen.tsx
+++ app/screens/TipScreens/HomeTipScreen.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import {View, Text} from 'react-native';
 // custom component
 import {StyledHeader} from '@/components';
+import {createStyleSheet, useStyles} from 'react-native-unistyles';
 
 const HomeTipScreen = () => {
+  const {styles} = useStyles(stylesheet);
   return (
-    <View>
+    <>
       <StyledHeader />
-    </View>
+      <View style={styles.mainContainer}></View>
+    </>
   );
 };
+
+const stylesheet = createStyleSheet(({colors}) => ({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: colors.backgroundColor,
+  },
+}));
 
 export default HomeTipScreen;
