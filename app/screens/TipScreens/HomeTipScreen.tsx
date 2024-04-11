1cfc72d2875b4aca5c33579a381d8b3b7ba6938f
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-12 00:54:21 +0530

    minor UIs applied

--- app/screens/TipScreens/HomeTipScreen.tsx
+++ app/screens/TipScreens/HomeTipScreen.tsx
@@ -1,23 +1,46 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 // custom component
-import {StyledHeader} from '@/components';
-import {createStyleSheet, useStyles} from 'react-native-unistyles';
+import { StyledHeader, StyledTextInput } from '@/components';
+import {
+  UnistylesRuntime,
+  createStyleSheet,
+  useStyles,
+} from 'react-native-unistyles';
 
 const HomeTipScreen = () => {
-  const {styles} = useStyles(stylesheet);
+  const { styles } = useStyles(stylesheet);
   return (
     <>
       <StyledHeader />
-      <View style={styles.mainContainer}></View>
+      <View style={styles.mainContainer}>
+        {/* Total Amount container */}
+        <View style={styles.totalAmountContainer}>
+          <Text style={styles.titleText}>TOTAL AMOUNT</Text>
+          <StyledTextInput keyboardType="numeric" returnKeyType="done" />
+        </View>
+      </View>
     </>
   );
 };
 
-const stylesheet = createStyleSheet(({colors}) => ({
+const stylesheet = createStyleSheet(({ colors }) => ({
   mainContainer: {
     flex: 1,
     backgroundColor: colors.backgroundColor,
+    paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
+  },
+  totalAmountContainer: {
+    width: '100%',
+    backgroundColor: '#444444',
+    paddingVertical: (UnistylesRuntime.screen.height * 2) / 100,
+    paddingHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
+    borderRadius: (UnistylesRuntime.screen.height * 1) / 100,
+  },
+  titleText: {
+    color: colors.primary_accent,
+    fontSize: 14,
+    fontWeight: '800',
   },
 }));
 
