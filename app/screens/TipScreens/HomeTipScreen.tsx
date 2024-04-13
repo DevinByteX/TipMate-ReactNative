60d752854449ed3851f8c37661e0540c1a2d0e46
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-13 21:47:41 +0530

    StyledBillContainer applied

--- app/screens/TipScreens/HomeTipScreen.tsx
+++ app/screens/TipScreens/HomeTipScreen.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 // custom component
-import { StyledHeader, StyledTextInput } from '@/components';
+import {
+  StyledBillContainer,
+  StyledHeader,
+  StyledTextInput,
+} from '@/components';
 import {
   UnistylesRuntime,
   createStyleSheet,
   useStyles,
 } from 'react-native-unistyles';
-
-const VerticalDevider = () => {
-  return (
-    <View
-      style={{
-        backgroundColor: '#4b4b4b',
-        height: '100%',
-        width: StyleSheet.hairlineWidth * 8,
-      }}
-    />
-  );
-};
 
 const HomeTipScreen = () => {
   const { styles } = useStyles(stylesheet);
@@ -37,167 +29,9 @@ const HomeTipScreen = () => {
           />
         </View>
         {/* Per Person Bill Container */}
-        <View style={styles.perPersonBillAmounts}>
-          <Text style={styles.perPersonBillAmountsTitleText}>PER PERSON</Text>
-          <View
-            style={{
-              flexDirection: 'row',
-              width: '100%',
-            }}>
-            <View
-              style={{
-                flex: 1,
-                paddingVertical: (UnistylesRuntime.screen.height * 2) / 100,
-                paddingHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-              }}>
-              <Text
-                style={{
-                  fontSize: 40,
-                  fontWeight: '800',
-                  color: 'white',
-                }}
-                adjustsFontSizeToFit={true}
-                allowFontScaling={false}
-                numberOfLines={1}>
-                $973.69
-              </Text>
-              <Text style={{ fontSize: 12, fontWeight: '600', color: 'white' }}>
-                Total
-              </Text>
-            </View>
-            <VerticalDevider />
-            <View
-              style={{
-                flex: 1,
-                paddingHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-                paddingVertical: (UnistylesRuntime.screen.height * 0.5) / 100,
-              }}>
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: 'flex-start',
-                  marginHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-                }}>
-                <Text
-                  style={{ fontSize: 12, fontWeight: '600', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  SUBTOTAL
-                </Text>
-                <Text
-                  style={{ fontSize: 14, fontWeight: '800', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  $84.95
-                </Text>
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: 'flex-end',
-                  marginHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-                }}>
-                <Text
-                  style={{ fontSize: 12, fontWeight: '600', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  TIP
-                </Text>
-                <Text
-                  style={{ fontSize: 14, fontWeight: '800', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  $12.74
-                </Text>
-              </View>
-            </View>
-          </View>
-        </View>
-
+        <StyledBillContainer />
         {/* Total Bill Container */}
-        <View style={styles.perPersonBillAmounts}>
-          <View
-            style={{
-              flexDirection: 'row',
-              width: '100%',
-            }}>
-            <View
-              style={{
-                flex: 1,
-                paddingVertical: (UnistylesRuntime.screen.height * 2) / 100,
-                paddingHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-              }}>
-              <Text
-                style={{
-                  fontSize: 40,
-                  fontWeight: '800',
-                  color: 'white',
-                }}
-                adjustsFontSizeToFit={true}
-                allowFontScaling={false}
-                numberOfLines={1}>
-                $973.69
-              </Text>
-              <Text style={{ fontSize: 12, fontWeight: '600', color: 'white' }}>
-                Total
-              </Text>
-            </View>
-            <VerticalDevider />
-            <View
-              style={{
-                flex: 1,
-                paddingHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-                paddingVertical: (UnistylesRuntime.screen.height * 0.5) / 100,
-              }}>
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: 'flex-start',
-                  marginHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-                }}>
-                <Text
-                  style={{ fontSize: 12, fontWeight: '600', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  SUBTOTAL
-                </Text>
-                <Text
-                  style={{ fontSize: 14, fontWeight: '800', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  $84.95
-                </Text>
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: 'flex-end',
-                  marginHorizontal: (UnistylesRuntime.screen.width * 3) / 100,
-                }}>
-                <Text
-                  style={{ fontSize: 12, fontWeight: '600', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  TIP
-                </Text>
-                <Text
-                  style={{ fontSize: 14, fontWeight: '800', color: 'white' }}
-                  adjustsFontSizeToFit={true}
-                  allowFontScaling={false}
-                  numberOfLines={1}>
-                  $12.74
-                </Text>
-              </View>
-            </View>
-          </View>
-        </View>
+        <StyledBillContainer />
       </View>
     </>
   );
