c5c135803c4598aa583de3ffee6c919c467b219a
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-13 15:12:20 +0530

    StyledBillContainer component initiated

---
+++ app/components/StyledBillContainer/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+export const StyledBillContainer = () => {
+  return (
+    <View>
+      <Text>StyledBillContainer</Text>
+    </View>
+  );
+};
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 export { CustomText } from "./CustomText";
 export { StyledHeader } from "./StyledHeader";
 export { StyledTextInput } from "./StyledTextInput";
+export { StyledBillContainer } from "./StyledBillContainer";
