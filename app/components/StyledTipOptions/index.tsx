fb951704d77a9ec68394eea5ae0c8cb1d7f4429e
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-14 18:06:18 +0530

    style config added

--- app/components/StyledTipOptions/index.tsx
+++ app/components/StyledTipOptions/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 export const StyledTipOptions = () => {
+  const { styles, theme } = useStyles(stylesheet);
   return (
-    <View>
+    <View style={styles.mainContainer}>
       <Text>StyledTipOptions</Text>
     </View>
   );
 };
+
+const stylesheet = createStyleSheet(({ colors }) => ({
+  mainContainer: {},
+}));
