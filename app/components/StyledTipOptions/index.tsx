924793a0fddadef924a37c16f27dec601b341bdc
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-15 11:32:37 +0530

    Style changes added

--- app/components/StyledTipOptions/index.tsx
+++ app/components/StyledTipOptions/index.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Pressable,
-} from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import {
   UnistylesRuntime,
   createStyleSheet,
@@ -26,14 +21,16 @@ const TipPercentageCapsule = ({
         {
           backgroundColor: active
             ? theme.colors.accent
-            : theme.colors.card,
+            : theme.colors.backgroundColor,
         },
       ]}>
-      <Text style={[styles.tipPercentageCapsuleText,        {
-          color: active
-            ? theme.colors.card
-            : theme.colors.card_typography,
-        }]}>{`${textValue}`}</Text>
+      <Text
+        style={[
+          styles.tipPercentageCapsuleText,
+          {
+            color: active ? theme.colors.card : theme.colors.card_typography,
+          },
+        ]}>{`${textValue}`}</Text>
     </Pressable>
   );
 };
@@ -42,9 +39,10 @@ export const StyledTipOptions = () => {
   const { styles, theme } = useStyles(stylesheet);
   return (
     <View style={styles.mainContainer}>
+      <Text style={styles.titleText}>{`${'SELECT TIP %'}`}</Text>
       <View style={styles.tipPercentageCapsuleContainer}>
         <TipPercentageCapsule textValue="5%" />
-        <TipPercentageCapsule textValue="10%" active/>
+        <TipPercentageCapsule textValue="10%" active />
         <TipPercentageCapsule textValue="15%" />
         <TipPercentageCapsule textValue="20%" />
       </View>
@@ -60,24 +58,32 @@ const stylesheet = createStyleSheet(({ colors }) => ({
   mainContainer: {
     width: '100%',
     borderRadius: (UnistylesRuntime.screen.height * 1) / 100,
+    backgroundColor: colors.card,
+    marginTop: (UnistylesRuntime.screen.height * 2) / 100,
+    paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
+    paddingVertical: (UnistylesRuntime.screen.height * 2) / 100,
+  },
+  titleText: {
+    color: colors.primary_accent,
+    fontSize: 14,
+    fontWeight: '800',
   },
   tipPercentageCapsuleContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginVertical: (UnistylesRuntime.screen.height * 2) / 100,
   },
   tipPercentageCapsule: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: (UnistylesRuntime.screen.height * 2) / 100,
+    width: (UnistylesRuntime.screen.width * 70) / 100 / 4,
     borderRadius: (UnistylesRuntime.screen.height * 1) / 100,
-    paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
     paddingVertical: (UnistylesRuntime.screen.height * 1.5) / 100,
   },
   tipPercentageCapsuleCustom: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.card,
-    marginTop: (UnistylesRuntime.screen.height * 2) / 100,
+    backgroundColor: colors.backgroundColor,
     borderRadius: (UnistylesRuntime.screen.height * 1) / 100,
     paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
     paddingVertical: (UnistylesRuntime.screen.height * 1.5) / 100,
