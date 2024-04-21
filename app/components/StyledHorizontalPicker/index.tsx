df096c1cd36288ae81b24cb00578dcda64fab96b
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-04-21 15:01:34 +0530

    StyledHorizontalPicker initiated

---
+++ app/components/StyledHorizontalPicker/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const StyledHorizontalPicker = () => {
+  return (
+    <View>
+      <Text>StyledHorizontalPicker</Text>
+    </View>
+  );
+};
--- app/components/index.ts
+++ app/components/index.ts
@@ -2,4 +2,5 @@ export { StyledText } from "./StyledText";
 export { StyledHeader } from "./StyledHeader";
 export { StyledTotalAmountInput } from "./StyledTotalAmountInput";
 export { StyledBillBox, VerticalDevider } from "./StyledBillBox";
-export { StyledTipOptions } from "./StyledTipOptions"
+export { StyledTipOptions } from "./StyledTipOptions";
+export { StyledHorizontalPicker } from "./StyledHorizontalPicker"
