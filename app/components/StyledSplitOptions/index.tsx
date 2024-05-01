b67dc98ee4ea5646165c6a53393c27217b8db776
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-05-01 18:35:11 +0530

    StyledSpiltOptions component initiated

---
+++ app/components/StyledSplitOptions/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+type StyledSpiltOptionsProps = {};
+
+export const StyledSpiltOptions = (props: StyledSpiltOptionsProps) => {
+  return (
+    <View>
+      <Text>StyledSpiltOptions</Text>
+    </View>
+  );
+};
--- app/components/index.ts
+++ app/components/index.ts
@@ -3,4 +3,5 @@ export { StyledHeader } from "./StyledHeader";
 export { StyledTotalAmountInput } from "./StyledTotalAmountInput";
 export { StyledBillBox, VerticalDevider } from "./StyledBillBox";
 export { StyledTipOptions } from "./StyledTipOptions";
-export { StyledHorizontalSlider } from "./StyledHorizontalSlider"
+export { StyledHorizontalSlider } from "./StyledHorizontalSlider";
+export { StyledSpiltOptions } from "./StyledSplitOptions";
