fda9238db693963e2eb6c0a117e933e6f592f4a3
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-05-03 01:06:51 +0530

    StyledSpiltOptions component partially completed

--- app/components/StyledSplitOptions/index.tsx
+++ app/components/StyledSplitOptions/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import {
+  UnistylesRuntime,
+  createStyleSheet,
+  useStyles,
+} from 'react-native-unistyles';
 
 type StyledSpiltOptionsProps = {};
 
 export const StyledSpiltOptions = (props: StyledSpiltOptionsProps) => {
+  const { styles, theme } = useStyles(stylesheet);
   return (
-    <View>
-      <Text>StyledSpiltOptions</Text>
+    <View style={styles.mainContainer}>
+      <Text style={styles.titleText}>{`SPLIT COUNT`}</Text>
     </View>
   );
 };
+
+const stylesheet = createStyleSheet(({ colors }) => ({
+  mainContainer: {
+    marginTop: (UnistylesRuntime.screen.height * 2) / 100,
+    width: '100%',
+    backgroundColor: colors.card,
+    paddingVertical: (UnistylesRuntime.screen.height * 2) / 100,
+    borderRadius: (UnistylesRuntime.screen.height * 1) / 100,
+  },
+  titleText: {
+    color: colors.primary_accent,
+    fontSize: 14,
+    fontWeight: '800',
+    paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
+  },
+}));
