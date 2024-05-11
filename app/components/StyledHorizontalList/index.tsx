84f079fa06828b03c92d4a0e4bad3e2717d17efb
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-05-11 17:33:03 +0530

    StyledHorizontalList component initiated

---
+++ app/components/StyledHorizontalList/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const StyledHorizontalList = () => {
+  return (
+    <View>
+      <Text>StyledHorizontalList</Text>
+    </View>
+  );
+};
--- app/components/index.ts
+++ app/components/index.ts
@@ -5,3 +5,4 @@ export { StyledBillBox, VerticalDevider } from "./StyledBillBox";
 export { StyledTipOptions } from "./StyledTipOptions";
 export { StyledHorizontalSlider } from "./StyledHorizontalSlider";
 export { StyledSpiltOptions } from "./StyledSplitOptions";
+export { StyledHorizontalList } from './StyledHorizontalList';
