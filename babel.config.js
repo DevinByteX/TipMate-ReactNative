caccd158c2cd4f6b20603d65620c389c4c39cbb0
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-05-18 10:51:49 +0530

    hooks path configuration added

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,8 @@ module.exports = {
           '@': './app',
           "@components": "./app/components/index", // if you are refering common index file in folder you should add "/index" end of the path
           "@styles": "./app/styles/",  // this is working either we add end "/" or not to the path, for safe side i have added end "/" to the path
-          "@navigation": "./app/navigation/"
+          "@navigation": "./app/navigation/",
+          "@hooks": "./app/hooks/index",
         }
       }
     ]
