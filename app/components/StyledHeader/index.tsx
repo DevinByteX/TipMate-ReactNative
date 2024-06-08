d41ac7b279cc1107f29e0c8469421c4c7a8224e2
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-08 16:24:51 +0530

    StyledHeader restyled again

--- app/components/StyledHeader/index.tsx
+++ app/components/StyledHeader/index.tsx
@@ -1,34 +1,75 @@
 import React from 'react';
 import { StatusBar, Text, View } from 'react-native';
 import { UnistylesRuntime, createStyleSheet, useStyles } from 'react-native-unistyles';
+import { StyledIcons } from '../StyledIcons';
 
-type styledHeaderProps = {};
+type styledHeaderProps = {
+  headerTitle: string;
+  headerSubTitle: string;
+};
 
-export const StyledHeader = ({}: styledHeaderProps) => {
+export const StyledHeader = ({
+  headerTitle = 'TipMate',
+  headerSubTitle = 'Smart Tips, Easy Living',
+}: styledHeaderProps) => {
   const { styles, theme } = useStyles(stylesheet);
 
   return (
-    <View style={styles.mainContainer}>
+    <>
       <StatusBar
         backgroundColor={theme.colors.headerBGColor}
         translucent={true}
         barStyle={UnistylesRuntime.colorScheme === 'dark' ? 'light-content' : 'dark-content'}
       />
       <View style={styles.headerMainContainer}>
-        <View style={styles.headerInnerContainer}></View>
+        <View style={styles.headerInnerContainer}>
+          <View style={styles.innerLeftContainer}>
+            <StyledIcons type={'FontAwesome'} name={'navicon'} size={styles.headerText.fontSize} />
+          </View>
+          <View style={styles.innerMiddleContainer}>
+            <Text style={styles.headerText}>{`${headerTitle}`}</Text>
+            <Text style={styles.headerSubText}>{`${headerSubTitle}`}</Text>
+          </View>
+          <View style={styles.innerRightContainer}>
+            <StyledIcons type={'Ionicons'} name={'save'} size={styles.headerText.fontSize} />
+          </View>
+        </View>
       </View>
-    </View>
+    </>
   );
 };
 
 const stylesheet = createStyleSheet(({ colors }) => ({
-  mainContainer: {
-    backgroundColor: colors.headerBGColor,
-    zIndex: 999,
-  },
   headerMainContainer: {
-    marginTop: UnistylesRuntime.statusBar.height,
+    backgroundColor: colors.headerBGColor,
+    paddingTop: UnistylesRuntime.statusBar.height,
+    paddingBottom: (UnistylesRuntime.screen.height * 0.5) / 100,
+  },
+  headerInnerContainer: {
+    flexDirection: 'row',
     height: (UnistylesRuntime.screen.height * 6) / 100,
   },
-  headerInnerContainer: {},
+  innerLeftContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingStart: (UnistylesRuntime.screen.width * 5) / 100,
+  },
+  innerMiddleContainer: { flex: 2, justifyContent: 'center', alignItems: 'center' },
+  innerRightContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    paddingEnd: (UnistylesRuntime.screen.width * 5) / 100,
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: '800',
+    color: colors.accent,
+    borderColor: colors.card_typography,
+  },
+  headerSubText: {
+    fontSize: 8,
+    fontWeight: '400',
+    color: colors.card_typography,
+  },
 }));
