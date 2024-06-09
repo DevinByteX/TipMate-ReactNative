6661ab5fade4de93bec45cf5405b402a6f818547
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-09 23:27:50 +0530

    react-navigation/drawer added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler'; // add the following at the top (make sure it's at the top and there's nothing else before it) of your entry file, such as index.js or App.js
 import { AppRegistry } from 'react-native';
 import App from './app/Entrypoint';
 import { name as appName } from './app.json';
