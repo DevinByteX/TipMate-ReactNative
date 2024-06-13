b8041625c79b37e30ba01af390707ce6bc21e90f
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-14 01:23:28 +0530

    drawerLabel and drawerActiveTintColor added

--- app/navigation/DrawerStack.tsx
+++ app/navigation/DrawerStack.tsx
@@ -1,16 +1,26 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useStyles } from 'react-native-unistyles';
 // Screens
 import HomeTipScreen from '@/screens/TipScreens/HomeTipScreen';
-import { StyledDrawer } from '@/components';
+import { StyledDrawer, StyledIcons } from '@/components';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerStack = () => {
+  const { theme } = useStyles();
+
   return (
     <Drawer.Navigator
       drawerContent={props => <StyledDrawer {...props} />}
       screenOptions={{ headerShown: false }}>
-      <Drawer.Screen name="Home" component={HomeTipScreen} />
+      <Drawer.Screen
+        name="Home"
+        component={HomeTipScreen}
+        options={{
+          drawerLabel: 'Tip & Split',
+          drawerActiveTintColor: theme.colors.accent,
+        }}
+      />
     </Drawer.Navigator>
   );
 };
