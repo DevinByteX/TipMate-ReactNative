62a700335db8e39d03c1a40bbea759c83200443d
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-15 17:35:41 +0530

    SavedTipsScreen initiated

---
+++ app/screens/TipScreens/SavedTipsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SavedTipsScreen = () => {
+  return (
+    <View>
+      <Text>SavedTipsScreen</Text>
+    </View>
+  );
+};
+
+export default SavedTipsScreen;
