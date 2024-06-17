a4aa0198a45a93b2ccd060ff52c4ace83e1fc66b
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-17 15:19:35 +0530

    New custom fonts added

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    project: {
+        ios: {},
+        android: {},
+    },
+    assets: ['./app/assets/fonts'],
     dependencies: {
         'react-native-vector-icons': {
             platforms: {
