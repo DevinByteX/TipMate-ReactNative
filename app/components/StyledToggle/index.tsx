46aa8d4399dbfb7f1d8c6152860840802c44644e
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-21 12:23:14 +0530

    Props and new improvements added

--- app/components/StyledToggle/index.tsx
+++ app/components/StyledToggle/index.tsx
@@ -1,44 +1,64 @@
 import React from 'react';
-import { View, Switch, Platform, SwitchProps } from 'react-native';
+import { View, Switch, Platform, SwitchProps, ViewStyle } from 'react-native';
 import { UnistylesRuntime, createStyleSheet, useStyles } from 'react-native-unistyles';
 
-type styledToggleProps = {
+// Define props to omit from ViewStyle that are specific to the Toggle component
+type OmitViewProps =
+  | 'backgroundColor'
+  | 'justifyContent'
+  | 'alignItems'
+  | 'height'
+  | 'width'
+  | 'borderRadius';
+
+// Create a new type that omits those props from ViewStyle
+type ToggleContainerStyle = Omit<ViewStyle, OmitViewProps>;
+
+type StyledToggleProps = {
   value?: boolean;
   disabled?: boolean;
   onChange?: SwitchProps['onChange'];
   onValueChange?: SwitchProps['onValueChange'];
+  toggleContainerStyles?: ToggleContainerStyle;
 };
 
-export const StyledToggle = ({ value, disabled, onChange, onValueChange }: styledToggleProps) => {
+export const StyledToggle = ({
+  value,
+  disabled,
+  onChange,
+  onValueChange,
+  toggleContainerStyles,
+}: StyledToggleProps) => {
   const { styles, theme } = useStyles(stylesheet);
+
+  const backgroundColor = disabled
+    ? theme.colors.disable_button
+    : value
+    ? theme.colors.accent
+    : theme.colors.devider;
+
+  const thumbColor = disabled
+    ? theme.colors.disable_button
+    : value
+    ? theme.colors.accent
+    : theme.colors.disable_button;
+
+  const trackColor = {
+    true: disabled ? theme.colors.disable_button : theme.colors.accent,
+    false: disabled ? theme.colors.disable_button : theme.colors.devider,
+  };
+
+  const iosBackgroundColor = disabled ? theme.colors.disable_button : theme.colors.card_typography;
+
   return (
-    <View
-      style={[
-        styles.buttonContainer,
-        {
-          backgroundColor: disabled
-            ? theme.colors.disable_button
-            : value
-            ? theme.colors.accent
-            : theme.colors.devider,
-        },
-      ]}>
+    <View style={[styles.buttonContainer, { backgroundColor }, toggleContainerStyles]}>
       <Switch
         style={styles.switchStyles}
         disabled={disabled}
-        thumbColor={
-          disabled
-            ? theme.colors.disable_button
-            : value
-            ? theme.colors.accent
-            : theme.colors.disable_button
-        }
+        thumbColor={thumbColor}
         value={value}
-        trackColor={{
-          true: disabled ? theme.colors.disable_button : theme.colors.accent,
-          false: disabled ? theme.colors.disable_button : theme.colors.devider,
-        }}
-        ios_backgroundColor={disabled ? theme.colors.disable_button : theme.colors.card_typography}
+        trackColor={trackColor}
+        ios_backgroundColor={iosBackgroundColor}
         onChange={onChange}
         onValueChange={onValueChange}
       />
