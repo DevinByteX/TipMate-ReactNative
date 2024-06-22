0c3f820e73d0e8e3593b8c7e026378c0ffdbfe6c
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-22 15:19:29 +0530

    unused code removed

--- app/styles/uniStyles.ts
+++ app/styles/uniStyles.ts
@@ -3,7 +3,6 @@ import { breakpoints } from './breakpoints';
 import { lightTheme, darkTheme, premiumTheme } from './themes';
 import { getUserPreferredTheme } from '@hooks';
 
-// Define types for breakpoints and themes
 type AppBreakpoints = typeof breakpoints;
 type AppThemes = {
     light: typeof lightTheme;
@@ -11,14 +10,11 @@ type AppThemes = {
     premium: typeof premiumTheme;
 };
 
-// Extend the Unistyles interfaces
 declare module 'react-native-unistyles' {
     export interface UnistylesThemes extends AppThemes { }
     export interface UnistylesBreakpoints extends AppBreakpoints { }
 }
 
-// For a basic setup without async storage (without using persisted user preferred theme), just add below code:
-// Note: This setup does not need to be wrapped in a self-executing function
 UnistylesRegistry
     .addThemes({
         light: lightTheme,
@@ -30,25 +26,3 @@ UnistylesRegistry
         initialTheme: 'light', // Default initial theme
         adaptiveThemes: false, // Change to true for system theme adaptation
     });
-
-/**
-// Self-executing async function for setting up UnistylesRegistry for using persisted user preferred theme
-(async () => {
-    try {
-        const initialTheme = await getUserPreferredTheme();
-        UnistylesRegistry
-            .addThemes({
-                light: lightTheme,
-                dark: darkTheme,
-                premium: premiumTheme,
-            })
-            .addBreakpoints(breakpoints)
-            .addConfig({
-                initialTheme: initialTheme,
-                adaptiveThemes: false, // Set to true if you want to adapt to system theme
-            });
-    } catch (error) {
-        console.log('Error setting up Unistyles:', error);
-    }
-})();
-*/
