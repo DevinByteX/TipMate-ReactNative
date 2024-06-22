978cd09391035879ec2bd2cfd7d43e6c6fec0d0e
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-22 15:37:49 +0530

    Preferred theme setting up function added

--- app/navigation/ApplicationNavigation.tsx
+++ app/navigation/ApplicationNavigation.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { useReduxDevToolsExtension } from '@react-navigation/devtools';
+import { UnistylesRuntime } from 'react-native-unistyles';
 // Custom Stacks
 import DrawerStack from '@navigation/DrawerStack';
+// Custom Hooks
+import { getUserPreferredTheme } from '@hooks';
 
 const ApplicationNavigator = (props: any) => {
+  useEffect(() => {
+    // Setting user preferred theme or initialTheme
+    const setAppTheme = async () => {
+      const preferredTheme = await getUserPreferredTheme();
+      if (!UnistylesRuntime.hasAdaptiveThemes) {
+        UnistylesRuntime.setTheme(preferredTheme);
+      }
+    };
+    setAppTheme();
+  }, []);
+
   const navigationRef = useNavigationContainerRef();
   useReduxDevToolsExtension(navigationRef);
 
