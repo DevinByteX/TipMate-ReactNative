690237904ef64bc49b19ff3b62e4faed589990b6
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-22 12:57:54 +0530

    setUserPreferredTheme hook added

--- app/hooks/asyncStorageHooks.ts
+++ app/hooks/asyncStorageHooks.ts
@@ -15,3 +15,11 @@ export const getUserPreferredTheme = async (): Promise<keyof UnistylesThemes> =>
     // Default to 'light' if theme is not valid or AsyncStorage error occurs
     return 'light';
 };
+
+export const setUserPreferredTheme = async (theme: keyof UnistylesThemes): Promise<void> => {
+    try {
+        await AsyncStorage.setItem('userPreferredTheme', theme);
+    } catch (error) {
+        console.error('Error setting user theme in AsyncStorage:', error);
+    }
+};
--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,7 +1,7 @@
 import { convertToTwoDecimalPoints } from './convertToTwoDecimals';
 import { calculateBillValues, BillCalculationType, RoundingMethodType, RoundingMethod, DisabledRoundingMethodsType } from './calculateBill';
 import { asyncStorageUtil } from './asyncStorageUtil';
-import { getUserPreferredTheme } from './asyncStorageHooks';
+import { getUserPreferredTheme, setUserPreferredTheme } from './asyncStorageHooks';
 
-export { convertToTwoDecimalPoints, calculateBillValues, RoundingMethod, asyncStorageUtil, getUserPreferredTheme };
+export { convertToTwoDecimalPoints, calculateBillValues, RoundingMethod, asyncStorageUtil, getUserPreferredTheme, setUserPreferredTheme };
 export type { BillCalculationType, RoundingMethodType, DisabledRoundingMethodsType }
