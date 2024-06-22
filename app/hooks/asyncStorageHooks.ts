6f37e9560cc542cf208b1c523539e4019abb15ab
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-22 12:41:08 +0530

    getUserPreferredTheme hook added

---
+++ app/hooks/asyncStorageHooks.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { UnistylesThemes } from "react-native-unistyles";
+
+// Function to fetch user preferred theme from AsyncStorage
+export const getUserPreferredTheme = async (): Promise<keyof UnistylesThemes> => {
+    try {
+        const theme = await AsyncStorage.getItem('userPreferredTheme');
+        if (theme === 'light' || theme === 'dark' || theme === 'premium') {
+            return theme as keyof UnistylesThemes;
+        }
+    } catch (error) {
+        console.error('Error fetching user theme from AsyncStorage:', error);
+    }
+
+    // Default to 'light' if theme is not valid or AsyncStorage error occurs
+    return 'light';
+};
--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,6 +1,7 @@
 import { convertToTwoDecimalPoints } from './convertToTwoDecimals';
 import { calculateBillValues, BillCalculationType, RoundingMethodType, RoundingMethod, DisabledRoundingMethodsType } from './calculateBill';
 import { asyncStorageUtil } from './asyncStorageUtil';
+import { getUserPreferredTheme } from './asyncStorageHooks';
 
-export { convertToTwoDecimalPoints, calculateBillValues, RoundingMethod, asyncStorageUtil };
+export { convertToTwoDecimalPoints, calculateBillValues, RoundingMethod, asyncStorageUtil, getUserPreferredTheme };
 export type { BillCalculationType, RoundingMethodType, DisabledRoundingMethodsType }
