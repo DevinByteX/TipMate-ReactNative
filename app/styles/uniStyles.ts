a2c1dd99336535e25e112813b83565cb43522ceb
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-22 14:03:47 +0530

    persisted user preferred theme usage also added and commented for future references

--- app/styles/uniStyles.ts
+++ app/styles/uniStyles.ts
@@ -1,29 +1,54 @@
 import { UnistylesRegistry } from 'react-native-unistyles';
 import { breakpoints } from './breakpoints';
 import { lightTheme, darkTheme, premiumTheme } from './themes';
+import { getUserPreferredTheme } from '@hooks';
 
-
-type AppBreakpoints = typeof breakpoints
+// Define types for breakpoints and themes
+type AppBreakpoints = typeof breakpoints;
 type AppThemes = {
-    light: typeof lightTheme
-    dark: typeof darkTheme
-    premium: typeof premiumTheme
-}
+    light: typeof lightTheme;
+    dark: typeof darkTheme;
+    premium: typeof premiumTheme;
+};
 
+// Extend the Unistyles interfaces
 declare module 'react-native-unistyles' {
     export interface UnistylesThemes extends AppThemes { }
-
     export interface UnistylesBreakpoints extends AppBreakpoints { }
 }
 
+// For a basic setup without async storage (without using persisted user preferred theme), just add below code:
+// Note: This setup does not need to be wrapped in a self-executing function
 UnistylesRegistry
     .addThemes({
         light: lightTheme,
         dark: darkTheme,
-        premium: premiumTheme
+        premium: premiumTheme,
     })
     .addBreakpoints(breakpoints)
     .addConfig({
-        initialTheme: 'light', // initial theme until user change it
-        adaptiveThemes: false, // adaptiveThemes will change themes according to the system
-    })
+        initialTheme: 'light', // Default initial theme
+        adaptiveThemes: false, // Change to true for system theme adaptation
+    });
+
+/**
+// Self-executing async function for setting up UnistylesRegistry for using persisted user preferred theme
+(async () => {
+    try {
+        const initialTheme = await getUserPreferredTheme();
+        UnistylesRegistry
+            .addThemes({
+                light: lightTheme,
+                dark: darkTheme,
+                premium: premiumTheme,
+            })
+            .addBreakpoints(breakpoints)
+            .addConfig({
+                initialTheme: initialTheme,
+                adaptiveThemes: false, // Set to true if you want to adapt to system theme
+            });
+    } catch (error) {
+        console.error('Error setting up Unistyles:', error);
+    }
+})();
+*/
