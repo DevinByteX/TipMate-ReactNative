cc893b6369650d434dd35963806228fe1944fa15
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-06-22 13:22:22 +0530

    import changed

--- app/hooks/asyncStorageHooks.ts
+++ app/hooks/asyncStorageHooks.ts
@@ -1,10 +1,10 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { UnistylesThemes } from "react-native-unistyles";
+import { asyncStorageUtil } from "@hooks";
 
 // Function to fetch user preferred theme from AsyncStorage
 export const getUserPreferredTheme = async (): Promise<keyof UnistylesThemes> => {
     try {
-        const theme = await AsyncStorage.getItem('userPreferredTheme');
+        const theme = await asyncStorageUtil.getData('userPreferredTheme');
         if (theme === 'light' || theme === 'dark' || theme === 'premium') {
             return theme as keyof UnistylesThemes;
         }
@@ -18,7 +18,7 @@ export const getUserPreferredTheme = async (): Promise<keyof UnistylesThemes> =>
 
 export const setUserPreferredTheme = async (theme: keyof UnistylesThemes): Promise<void> => {
     try {
-        await AsyncStorage.setItem('userPreferredTheme', theme);
+        await asyncStorageUtil.saveData('userPreferredTheme', theme);
     } catch (error) {
         console.error('Error setting user theme in AsyncStorage:', error);
     }
