02216c7576851b40ae32385f921dd66e8c9a3a1d
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-08-13 01:46:15 +0530

    toastConfig moved and added topOffset and some code refactorings

--- app/navigation/ApplicationNavigation.tsx
+++ app/navigation/ApplicationNavigation.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
+// Third party libs
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { useReduxDevToolsExtension } from '@react-navigation/devtools';
 import { UnistylesRuntime } from 'react-native-unistyles';
+import Toast from 'react-native-toast-message';
 // Custom Stacks
 import DrawerStack from '@navigation/DrawerStack';
 // Custom Hooks
 import { getUserPreferredTheme } from '@hooks';
+import { toastConfig } from '@styles/toastConfig';
 
 const ApplicationNavigator = (props: any) => {
   useEffect(() => {
@@ -28,6 +31,7 @@ const ApplicationNavigator = (props: any) => {
       <NavigationContainer ref={navigationRef}>
         <DrawerStack />
       </NavigationContainer>
+      <Toast config={toastConfig} topOffset={UnistylesRuntime.insets.top} />
     </SafeAreaProvider>
   );
 };
