93c3ec2810c0a3ab6a86a887aa1e0053d74253cb
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-08-13 17:19:01 +0530

    toastConfig updated with unistyles

--- app/styles/toastConfig.tsx
+++ app/styles/toastConfig.tsx
@@ -6,6 +6,7 @@ import {
   ErrorToast,
   ToastConfigParams,
 } from 'react-native-toast-message';
+import { createStyleSheet, UnistylesRuntime, useStyles } from 'react-native-unistyles';
 
 /*
   1. Create the config
@@ -15,53 +16,76 @@ export const toastConfig = {
     Overwrite 'success' type,
     by modifying the existing `BaseToast` component
   */
-  success: (props: BaseToastProps) => (
-    <BaseToast
-      {...props}
-      style={{ borderLeftColor: 'pink', backgroundColor: 'red' }}
-      contentContainerStyle={{ paddingHorizontal: 15 }}
-      text1Style={{
-        fontSize: 15,
-        fontWeight: '400',
-      }}
-    />
-  ),
+  success: (props: BaseToastProps) => {
+    const { styles } = useStyles(stylesheet);
+    return (
+      <BaseToast
+        {...props}
+        style={styles.successToastStyle}
+        contentContainerStyle={styles.successToastContainerStyle}
+        text1Style={styles.successText1}
+      />
+    );
+  },
+
   /*
     Overwrite 'error' type,
     by modifying the existing `ErrorToast` component
   */
-  error: (props: BaseToastProps) => (
-    <ErrorToast
-      {...props}
-      text1Style={{
-        fontSize: 17,
-      }}
-      text2Style={{
-        fontSize: 15,
-      }}
-    />
-  ),
+  error: (props: BaseToastProps) => {
+    const { styles } = useStyles(stylesheet);
+    return (
+      <ErrorToast
+        {...props}
+        style={styles.errorToastStyle}
+        contentContainerStyle={styles.errorToastContainerStyle}
+        text1Style={styles.errorText1}
+      />
+    );
+  },
+
   /*
     Or create a completely new type - `tomatoToast`,
     building the layout from scratch.
-  
     I can consume any custom `props` I want.
     They will be passed when calling the `show` method (see below)
+    *** USAGE : Toast.show({ type: 'tomatoToast', text1: `example text`, props:{ uuid :'example uuid'}, });
   */
-  tomatoToast: ({ text1, props }: ToastConfigParams<any>) => (
-    <View style={{ height: 60, width: '100%', backgroundColor: 'tomato' }}>
-      <Text>{text1}</Text>
-      {props?.uuid && <Text>{props.uuid}</Text>}
-    </View>
-  ),
-  /*
-    tomatoToast usage :
-    Toast.show({
-        type: 'tomatoToast',
-        text1: `example text`,
-        props:{
-          uuid :'example uuid'
-        }
-      });
-  */
+  tomatoToast: ({ text1, props }: ToastConfigParams<any>) => {
+    const { styles } = useStyles(stylesheet);
+    return (
+      <View style={styles.tomatoToastStyle}>
+        <Text>{text1}</Text>
+        {props?.uuid && <Text>{props.uuid}</Text>}
+      </View>
+    );
+  },
 };
+
+const stylesheet = createStyleSheet(({ colors, fonts }) => ({
+  successToastStyle: {
+    borderLeftColor: colors.accent,
+    backgroundColor: colors.backgroundColor,
+    borderColor: colors.card,
+    borderWidth: UnistylesRuntime.hairlineWidth,
+  },
+  successToastContainerStyle: {
+    paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
+  },
+  successText1: { color: colors.accent, fontFamily: fonts.Nunito_Black, fontSize: 16 },
+  errorToastStyle: {
+    borderLeftColor: colors.error_toast,
+    backgroundColor: colors.backgroundColor,
+    borderColor: colors.card,
+    borderWidth: UnistylesRuntime.hairlineWidth,
+  },
+  errorToastContainerStyle: {
+    paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
+  },
+  errorText1: { color: colors.error_toast, fontFamily: fonts.Nunito_Black, fontSize: 16 },
+  tomatoToastStyle: {
+    height: (UnistylesRuntime.screen.height * 8) / 100,
+    width: '100%',
+    backgroundColor: colors.error_toast,
+  },
+}));
