0c993c6599b05f6ee2cb3f36636d5c8a42e300d5
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-08-22 00:27:26 +0530

    essential import added as per FAQ of RN-Unistyles

--- app/Entrypoint.tsx
+++ app/Entrypoint.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-native-unistyles'; // Per the Unitstyles FAQ, add this configuration in the root file (e.g., `app.js` or `index.js`)a
 import '@styles/uniStyles'; // This should always be imported in the root file of the app, such as app.js or index.
 import ApplicationNavigator from '@navigation/ApplicationNavigation';
 import { AppProvider } from './context/AppContext';
