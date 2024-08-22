16eb90b2e8b95b648c90a88614b642fd24c2e217
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-08-22 11:38:00 +0530

    wrapped with GestureHandlerRootView as per the react-native-gesture-handler documentation

--- app/Entrypoint.tsx
+++ app/Entrypoint.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import 'react-native-unistyles'; // Per the Unitstyles FAQ, add this configuration in the root file (e.g., `app.js` or `index.js`)a
 import '@styles/uniStyles'; // This should always be imported in the root file of the app, such as app.js or index.
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import ApplicationNavigator from '@navigation/ApplicationNavigation';
 import { AppProvider } from './context/AppContext';
 
+/**
+ * important to keep the GestureHandlerRootView as close to the actual root view as possible.
+ * Root means the very top of your component tree.
+ * if you want it to fill the screen, you will need to pass { flex: 1 } like you'll need to do with a normal View
+ */
+
 const Entrypoint = () => {
   return (
-    <AppProvider>
-      <ApplicationNavigator />
-    </AppProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppProvider>
+        <ApplicationNavigator />
+      </AppProvider>
+    </GestureHandlerRootView>
   );
 };
 
