bc0327746440a389dc7dcf9a860f1fee60a80b8d
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-08-26 01:29:01 +0530

    CustomThemesConfig converted into function which no longer need to pass styles as a prop

--- app/configs/customThemesConfig.ts
+++ app/configs/customThemesConfig.ts
@@ -1,5 +1,5 @@
 import { CustomisedTheme } from "@hooks";
-import { UnistylesThemes } from "react-native-unistyles";
+import { UnistylesThemes, useStyles } from "react-native-unistyles";
 
 export interface ThemeBox {
     label: string;
@@ -7,37 +7,40 @@ export interface ThemeBox {
     customisedTheme: CustomisedTheme[];
 }
 
-export const CustomThemesConfig = (themeColors: any): ThemeBox[] => [
-    {
-        label: 'theme_first',
-        buttonColor: themeColors.accent_first,
-        customisedTheme: [
-            { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_light } },
-            { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_dark } },
-        ],
-    },
-    {
-        label: 'theme_second',
-        buttonColor: themeColors.accent_second,
-        customisedTheme: [
-            { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_light_second } },
-            { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_dark_second } },
-        ],
-    },
-    {
-        label: 'theme_third',
-        buttonColor: themeColors.accent_third,
-        customisedTheme: [
-            { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_light_third } },
-            { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_dark_third } },
-        ],
-    },
-    {
-        label: 'theme_fourth',
-        buttonColor: themeColors.accent_forth,
-        customisedTheme: [
-            { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_light_fourth } },
-            { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: themeColors.primary_accent_dark_fourth } },
-        ],
-    },
-];
+export const CustomThemesConfig = (): ThemeBox[] => {
+    const { theme } = useStyles()
+    return [
+        {
+            label: 'theme_first',
+            buttonColor: theme.colors.accent_first,
+            customisedTheme: [
+                { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_light } },
+                { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_dark } },
+            ],
+        },
+        {
+            label: 'theme_second',
+            buttonColor: theme.colors.accent_second,
+            customisedTheme: [
+                { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_light_second } },
+                { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_dark_second } },
+            ],
+        },
+        {
+            label: 'theme_third',
+            buttonColor: theme.colors.accent_third,
+            customisedTheme: [
+                { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_light_third } },
+                { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_dark_third } },
+            ],
+        },
+        {
+            label: 'theme_fourth',
+            buttonColor: theme.colors.accent_forth,
+            customisedTheme: [
+                { themeName: 'light' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_light_fourth } },
+                { themeName: 'dark' as keyof UnistylesThemes, customColors: { accent: theme.colors.primary_accent_dark_fourth } },
+            ],
+        },
+    ]
+};
