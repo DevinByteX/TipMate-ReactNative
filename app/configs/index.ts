5cb7fcf0a93244575a4ec20ba72b0753aa60392c
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-10-12 17:39:45 +0530

    CurrencyType exported

--- app/configs/index.ts
+++ app/configs/index.ts
@@ -1,5 +1,5 @@
-import { Constants } from './constants';
+import { Constants, CurrencyType } from './constants';
 import { CustomThemesConfig, ThemeBox } from './customThemesConfig';
 
 export { Constants, CustomThemesConfig };
-export type { ThemeBox };
+export type { ThemeBox, CurrencyType };
