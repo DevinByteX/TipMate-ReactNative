6f0b92c86b80591074a69be4919aeb16d3c1fa54
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-10-12 17:40:17 +0530

    currencies prop added and unnecessary props removed

--- app/components/StyledCurrencySelector/index.tsx
+++ app/components/StyledCurrencySelector/index.tsx
@@ -2,58 +2,9 @@ import React, { useState } from 'react';
 import { View, Text, Modal, Pressable, ScrollView } from 'react-native';
 import { createStyleSheet, UnistylesRuntime, useStyles } from 'react-native-unistyles';
 import { StyledIcons } from '@components';
+import { type CurrencyType } from '@configs';
 
-const currencies = [
-  { currencyId: 'AED', currencyName: 'UAE Dirham', currencySign: 'AED' },
-  { currencyId: 'ARS', currencyName: 'Argentine Peso', currencySign: 'ARS' },
-  { currencyId: 'AUD', currencyName: 'Australian Dollar', currencySign: '$' },
-  { currencyId: 'BDT', currencyName: 'Bangladeshi Taka', currencySign: '৳' },
-  { currencyId: 'BHD', currencyName: 'Bahraini Dinar', currencySign: 'BHD' },
-  { currencyId: 'BRL', currencyName: 'Brazilian Real', currencySign: 'R$' },
-  { currencyId: 'CAD', currencyName: 'Canadian Dollar', currencySign: '$' },
-  { currencyId: 'CHF', currencyName: 'Swiss Franc', currencySign: 'CHF' },
-  { currencyId: 'CLP', currencyName: 'Chilean Peso', currencySign: 'CLP' },
-  { currencyId: 'CNY', currencyName: 'Chinese Yuan', currencySign: '¥' },
-  { currencyId: 'EGP', currencyName: 'Egyptian Pound', currencySign: '£' },
-  { currencyId: 'EUR', currencyName: 'Euro', currencySign: '€' },
-  { currencyId: 'GHS', currencyName: 'Ghanaian Cedi', currencySign: 'GHS' },
-  { currencyId: 'GBP', currencyName: 'British Pound Sterling', currencySign: '£' },
-  { currencyId: 'IDR', currencyName: 'Indonesian Rupiah', currencySign: 'Rp' },
-  { currencyId: 'ILS', currencyName: 'Israeli New Shekel', currencySign: '₪' },
-  { currencyId: 'INR', currencyName: 'Indian Rupee', currencySign: '₹' },
-  { currencyId: 'JOD', currencyName: 'Jordanian Dinar', currencySign: 'JOD' },
-  { currencyId: 'JPY', currencyName: 'Japanese Yen', currencySign: '¥' },
-  { currencyId: 'KES', currencyName: 'Kenyan Shilling', currencySign: 'KES' },
-  { currencyId: 'KRW', currencyName: 'South Korean Won', currencySign: '₩' },
-  { currencyId: 'KWD', currencyName: 'Kuwaiti Dinar', currencySign: 'KWD' },
-  { currencyId: 'LKR', currencyName: 'Sri Lankan Rupee', currencySign: 'LKR' },
-  { currencyId: 'MXN', currencyName: 'Mexican Peso', currencySign: '$' },
-  { currencyId: 'MYR', currencyName: 'Malaysian Ringgit', currencySign: 'RM' },
-  { currencyId: 'NGN', currencyName: 'Nigerian Naira', currencySign: '₦' },
-  { currencyId: 'NZD', currencyName: 'New Zealand Dollar', currencySign: '$' },
-  { currencyId: 'OMR', currencyName: 'Omani Rial', currencySign: 'OMR' },
-  { currencyId: 'PHP', currencyName: 'Philippine Peso', currencySign: '₱' },
-  { currencyId: 'PKR', currencyName: 'Pakistani Rupee', currencySign: '₨' },
-  { currencyId: 'QAR', currencyName: 'Qatari Riyal', currencySign: 'QAR' },
-  { currencyId: 'RUB', currencyName: 'Russian Ruble', currencySign: '₽' },
-  { currencyId: 'SAR', currencyName: 'Saudi Riyal', currencySign: 'SAR' },
-  { currencyId: 'SGD', currencyName: 'Singapore Dollar', currencySign: '$' },
-  { currencyId: 'THB', currencyName: 'Thai Baht', currencySign: '฿' },
-  { currencyId: 'TRY', currencyName: 'Turkish Lira', currencySign: '₺' },
-  { currencyId: 'TZS', currencyName: 'Tanzanian Shilling', currencySign: 'TZS' },
-  { currencyId: 'UGX', currencyName: 'Ugandan Shilling', currencySign: 'UGX' },
-  { currencyId: 'USD', currencyName: 'US Dollar', currencySign: '$' },
-  { currencyId: 'VND', currencyName: 'Vietnamese Dong', currencySign: '₫' },
-  { currencyId: 'ZAR', currencyName: 'South African Rand', currencySign: 'R' },
-];
-
-type CurrencyType = {
-  currencyId: string;
-  currencyName: string;
-  currencySign: string;
-};
-
-const CurrencySelectiveScroll = ({ currencies }: { currencies: CurrencyType[] }) => {
+const CurrencySelectiveScroll = ({ currencies }: { currencies: CurrencyType[] | undefined }) => {
   const { styles, theme } = useStyles(stylesheet);
 
   return (
@@ -61,7 +12,7 @@ const CurrencySelectiveScroll = ({ currencies }: { currencies: CurrencyType[] })
       contentContainerStyle={styles.currencyScrollContainerStyles}
       showsHorizontalScrollIndicator={false}
       showsVerticalScrollIndicator={false}>
-      {currencies.map(currency => (
+      {currencies?.map(currency => (
         <View key={currency.currencyId} style={styles.modalContentCurrencyBarContainer}>
           <View style={styles.currencySelectiveName}>
             <Text style={styles.modalcurrencyText}>{currency.currencyName}</Text>
@@ -77,9 +28,11 @@ const CurrencySelectiveScroll = ({ currencies }: { currencies: CurrencyType[] })
 
 const CurrencyListModal = ({
   modalVisibility,
+  currencies,
   closeButtonPress,
 }: {
   modalVisibility?: boolean;
+  currencies?: CurrencyType[];
   closeButtonPress?: () => void;
 }) => {
   const { styles, theme } = useStyles(stylesheet);
