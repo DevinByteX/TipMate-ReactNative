24248df91e9d2bffa08f88643af21686d4865735
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-10-12 17:32:38 +0530

    CurrencySelectiveScroll UI completed

--- app/components/StyledCurrencySelector/index.tsx
+++ app/components/StyledCurrencySelector/index.tsx
@@ -1,7 +1,79 @@
 import React, { useState } from 'react';
-import { View, Text, Modal, Pressable } from 'react-native';
+import { View, Text, Modal, Pressable, ScrollView } from 'react-native';
 import { createStyleSheet, UnistylesRuntime, useStyles } from 'react-native-unistyles';
 import { StyledIcons } from '@components';
+
+const currencies = [
+  { currencyId: 'AED', currencyName: 'UAE Dirham', currencySign: 'AED' },
+  { currencyId: 'ARS', currencyName: 'Argentine Peso', currencySign: 'ARS' },
+  { currencyId: 'AUD', currencyName: 'Australian Dollar', currencySign: '$' },
+  { currencyId: 'BDT', currencyName: 'Bangladeshi Taka', currencySign: '৳' },
+  { currencyId: 'BHD', currencyName: 'Bahraini Dinar', currencySign: 'BHD' },
+  { currencyId: 'BRL', currencyName: 'Brazilian Real', currencySign: 'R$' },
+  { currencyId: 'CAD', currencyName: 'Canadian Dollar', currencySign: '$' },
+  { currencyId: 'CHF', currencyName: 'Swiss Franc', currencySign: 'CHF' },
+  { currencyId: 'CLP', currencyName: 'Chilean Peso', currencySign: 'CLP' },
+  { currencyId: 'CNY', currencyName: 'Chinese Yuan', currencySign: '¥' },
+  { currencyId: 'EGP', currencyName: 'Egyptian Pound', currencySign: '£' },
+  { currencyId: 'EUR', currencyName: 'Euro', currencySign: '€' },
+  { currencyId: 'GHS', currencyName: 'Ghanaian Cedi', currencySign: 'GHS' },
+  { currencyId: 'GBP', currencyName: 'British Pound Sterling', currencySign: '£' },
+  { currencyId: 'IDR', currencyName: 'Indonesian Rupiah', currencySign: 'Rp' },
+  { currencyId: 'ILS', currencyName: 'Israeli New Shekel', currencySign: '₪' },
+  { currencyId: 'INR', currencyName: 'Indian Rupee', currencySign: '₹' },
+  { currencyId: 'JOD', currencyName: 'Jordanian Dinar', currencySign: 'JOD' },
+  { currencyId: 'JPY', currencyName: 'Japanese Yen', currencySign: '¥' },
+  { currencyId: 'KES', currencyName: 'Kenyan Shilling', currencySign: 'KES' },
+  { currencyId: 'KRW', currencyName: 'South Korean Won', currencySign: '₩' },
+  { currencyId: 'KWD', currencyName: 'Kuwaiti Dinar', currencySign: 'KWD' },
+  { currencyId: 'LKR', currencyName: 'Sri Lankan Rupee', currencySign: 'LKR' },
+  { currencyId: 'MXN', currencyName: 'Mexican Peso', currencySign: '$' },
+  { currencyId: 'MYR', currencyName: 'Malaysian Ringgit', currencySign: 'RM' },
+  { currencyId: 'NGN', currencyName: 'Nigerian Naira', currencySign: '₦' },
+  { currencyId: 'NZD', currencyName: 'New Zealand Dollar', currencySign: '$' },
+  { currencyId: 'OMR', currencyName: 'Omani Rial', currencySign: 'OMR' },
+  { currencyId: 'PHP', currencyName: 'Philippine Peso', currencySign: '₱' },
+  { currencyId: 'PKR', currencyName: 'Pakistani Rupee', currencySign: '₨' },
+  { currencyId: 'QAR', currencyName: 'Qatari Riyal', currencySign: 'QAR' },
+  { currencyId: 'RUB', currencyName: 'Russian Ruble', currencySign: '₽' },
+  { currencyId: 'SAR', currencyName: 'Saudi Riyal', currencySign: 'SAR' },
+  { currencyId: 'SGD', currencyName: 'Singapore Dollar', currencySign: '$' },
+  { currencyId: 'THB', currencyName: 'Thai Baht', currencySign: '฿' },
+  { currencyId: 'TRY', currencyName: 'Turkish Lira', currencySign: '₺' },
+  { currencyId: 'TZS', currencyName: 'Tanzanian Shilling', currencySign: 'TZS' },
+  { currencyId: 'UGX', currencyName: 'Ugandan Shilling', currencySign: 'UGX' },
+  { currencyId: 'USD', currencyName: 'US Dollar', currencySign: '$' },
+  { currencyId: 'VND', currencyName: 'Vietnamese Dong', currencySign: '₫' },
+  { currencyId: 'ZAR', currencyName: 'South African Rand', currencySign: 'R' },
+];
+
+type CurrencyType = {
+  currencyId: string;
+  currencyName: string;
+  currencySign: string;
+};
+
+const CurrencySelectiveScroll = ({ currencies }: { currencies: CurrencyType[] }) => {
+  const { styles, theme } = useStyles(stylesheet);
+
+  return (
+    <ScrollView
+      contentContainerStyle={styles.currencyScrollContainerStyles}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}>
+      {currencies.map(currency => (
+        <View key={currency.currencyId} style={styles.modalContentCurrencyBarContainer}>
+          <View style={styles.currencySelectiveName}>
+            <Text style={styles.modalcurrencyText}>{currency.currencyName}</Text>
+          </View>
+          <View style={styles.currencySelectiveSign}>
+            <Text style={styles.modalcurrencyText}>{currency.currencySign}</Text>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+};
 
 const CurrencyListModal = ({
   modalVisibility,
@@ -27,7 +99,7 @@ const CurrencyListModal = ({
           </Pressable>
         </View>
         <View style={styles.modalContentContainer}>
-          <View style={styles.modalContentButtonContainer}></View>
+          <CurrencySelectiveScroll currencies={currencies} />
         </View>
       </View>
     </Modal>
@@ -133,7 +205,7 @@ const stylesheet = createStyleSheet(({ colors, fonts, utils }) => ({
     borderWidth: UnistylesRuntime.hairlineWidth * 2,
     borderColor: colors.accent,
     backgroundColor: utils.hexToRGBA(colors.card, 0.95),
-    bottom: 0,
+    bottom: -(UnistylesRuntime.insets.bottom * 0.2), // Minus
     position: 'absolute',
   },
   modalTitleAndCloseButtonContainer: {
@@ -151,10 +223,32 @@ const stylesheet = createStyleSheet(({ colors, fonts, utils }) => ({
     width: '100%',
     paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
   },
-  modalContentButtonContainer: {
+  modalContentCurrencyBarContainer: {
+    flexDirection: 'row',
     width: '100%',
-    paddingVertical: (UnistylesRuntime.screen.height * 1) / 100,
+    paddingVertical: (UnistylesRuntime.screen.height * 1.25) / 100,
+    paddingHorizontal: (UnistylesRuntime.screen.width * 5) / 100,
     backgroundColor: colors.backgroundColor,
     borderRadius: (UnistylesRuntime.screen.height * 1) / 100,
+  },
+  modalcurrencyText: {
+    fontSize: 14,
+    fontFamily: fonts.Montserrat_Black,
+    color: colors.card_typography,
+  },
+  currencySelectiveName: {
+    flex: 4,
+    justifyContent: 'center',
+    paddingVertical: (UnistylesRuntime.screen.height * 0.5) / 100,
+  },
+  currencySelectiveSign: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: (UnistylesRuntime.screen.height * 0.5) / 100,
+  },
+  currencyScrollContainerStyles: {
+    gap: (UnistylesRuntime.screen.height * 1) / 100,
+    paddingBottom: UnistylesRuntime.insets.bottom * 3,
   },
 }));
