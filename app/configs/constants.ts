40c35d98d170d1a722e00dbb562fc275ab44d596
Devin Godage <devinkanilka@gmail.com>
2025-06-16 02:10:41 +0530

    feat(constants): add APP_LINKS, EMAILS, and APP_INFO constants for app configuration

--- app/configs/constants.ts
+++ app/configs/constants.ts
@@ -79,3 +79,16 @@ export const Constants = {
     defaultCurrencyObject:
         { currencyId: 'USD', currencyName: 'US Dollar', currencySign: '$' } as CurrencyType,
 };
+
+export const APP_LINKS = {
+    playStore: 'https://play.google.com/store/apps/dev?id=7058400265435813852',
+    privacyPolicy: 'https://www.freeprivacypolicy.com/live/7f96b8fd-6f24-4098-a929-443d12cb5f54',
+};
+
+export const EMAILS = {
+    support: 'devinforge.appsconsole@gmail.com',
+};
+
+export const APP_INFO = {
+    version: '1.0.1',
+};
