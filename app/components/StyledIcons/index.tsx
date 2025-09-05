d60d05600f267a1cc2c0cc969c671c671de632ae
Devin Godage <devinkanilka@gmail.com>
2025-09-06 00:37:32 +0530

    chore: remove react-native-vector-icons dependency and related files
    Clean up project by removing unused vector icons package and its configuration

--- app/components/StyledIcons/index.tsx
+++ app/components/StyledIcons/index.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import { TextStyle, ViewStyle, TextProps } from 'react-native';
 // Vector Icons
-import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Entypo from 'react-native-vector-icons/Entypo';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import Octicons from 'react-native-vector-icons/Octicons';
-import Foundation from 'react-native-vector-icons/Foundation';
-import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 // Object containing all icon types for easy reference
 export const StyledIconTypes = {
-  MaterialCommunityIcons,
-  MaterialIcons,
   Ionicons,
   Feather,
   FontAwesome,
   FontAwesome5,
-  AntDesign,
   Entypo,
-  SimpleLineIcons,
   Octicons,
-  Foundation,
-  EvilIcons,
 };
 
 // Type representing keys of StyledIconTypes
--- react-native.config.js
+++ react-native.config.js
@@ -5,10 +5,6 @@ module.exports = {
     },
     assets: ['./app/assets/fonts'],
     dependencies: {
-        'react-native-vector-icons': {
-            platforms: {
-                ios: null,
-            },
-        },
+
     },
 };
